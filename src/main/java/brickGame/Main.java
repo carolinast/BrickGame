5b86068d4d5cdc4b18edbd96eed1e3a3f3c59312
Karolina <krk.carolina@gmail.com>
2019-07-21 14:42:42 +0200

    demo complete

--- src/main/java/brickGame/Main.java
+++ src/main/java/brickGame/Main.java
@@ -10,8 +10,7 @@ public class Main {
         JFrame obj = new JFrame();
         Gameplay gameplay = new Gameplay();
 
-        obj.setBounds(50, 50, 1000, 800);
-        obj.setBackground(Color.YELLOW);
+        obj.setBounds(10, 10, 700, 600);
         obj.setVisible(true);
         obj.setResizable(false);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
